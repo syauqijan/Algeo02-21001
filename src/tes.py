b82820d1830cb9b7ce74c947ad49784e6b9e7080
syauqijan <13521014@std.stei.itb.ac.id>
2022-11-13 22:36:26 +0700

    update function

--- src/tes.py
+++ src/tes.py
@@ -8,19 +8,26 @@ def load_images_from_folder(folder):
     images = []
     for filename in os.listdir(folder):
         img = cv2.imread(os.path.join(folder,filename),0)
-        resized = cv2.resize(img, (256, 256))
         if img is not None:
             images.append(img)
-            
-            printmatrix(resized)
+    return images
+
 #         cv2.imshow('image', resized)   #ini kalo mau ngeprint gambarnya
 #         cv2.waitKey(0)
 #         cv2.destroyAllWindows()
 
-# tes = load_images_from_folder(path)
-# print(tes)
+
+def resize_image(folder):
+    resized_images = []
+    for image in load_images_from_folder(folder):
+        resized = cv2.resize(image, (256, 256))
+        resized_images.append(resized)
+    return resized_images
+
 path = 'test/dataset/pins_Zendaya'
-load_images_from_folder(path)
+matrix_img = resize_image(path)
+printmatrix(matrix_img)
+
 
 
 
