840683450f835851c30a697b4e9c3968ab16090e
kelvinra <13521005@mahasiswa.itb.ac.id>
2022-11-21 23:55:43 +0700

    add function

---
+++ src/testing/eucidilian.py
@@ -0,0 +1,56 @@
+from step1 import *
+from eigenfaces import *
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+def weighttest(resultarr,testimg):
+    wtest = [[0 for i in range(256)] for j in range(256)]
+    for i in range (len(resultarr)):
+        wtest = np.add(wtest,(np.matmul(testimg,resultarr[i])))
+    return wtest
+
+
+def weightdata(differenceimg,resultarr):
+    w = []
+    for i  in range(len(differenceimg)):
+        temp = [[0 for i in range(256)] for j in range(256)] 
+        for j in range(len(resultarr)):
+            temp = (np.add(temp,(np.matmul(differenceimg[i],resultarr[j]))))
+        w.append(temp)
+    return w
+
+
+# plt.imshow(resultarr[0], cmap = 'gray')
+# plt.show()
+def euclidean_distance(x, y):
+    sum = 0
+    for i in range(256):
+        # print("a")
+        for j in range(256):
+            x[i][j] = x[i][j] - y[i][j]
+    for i in range(256):
+        for j in range(256):
+            sum += x[i][j]**2
+    return np.sqrt(sum)
+   
+
+def main(wtest, w):
+    h = 0
+    min = euclidean_distance(wtest,w[0])
+    for i in range(1, len(w)):
+        distance = euclidean_distance(wtest,w[i])
+        print(distance)
+        if distance < min :
+            min = distance
+            h = i
+    return h
+
+def showimg(images,h):
+    images[h] = reshapeimg(images[h])
+    # plt.imshow(images[h], cmap = 'RGB')
+    # plt.show()
+    images[h] = np.array(images[h])
+    cv2.imshow('image', images[h])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
